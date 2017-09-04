92382e52997b36575208966fda932dafe3511797
ThomasGroenewegen <thomasgroenewegen@hotmail.com>
2017-09-04 12:38:49 +0200

    Added OrderService (interface) and OrderServiceHibernate (implementation) to create package structure

---
+++ src/main/java/workshop2/persistencelayer/hibernate/OrderServiceHibernate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop2.persistencelayer.hibernate;
+
+import workshop2.persistencelayer.OrderService;
+
+/**
+ *
+ * @author thoma
+ */
+public class OrderServiceHibernate implements OrderService {
+    
+}
