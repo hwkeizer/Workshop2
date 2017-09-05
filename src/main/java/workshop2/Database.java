566650a5a7b3f9f8e4d58e0f874d9e7b01ca0e91
ThomasGroenewegen <thomasgroenewegen@hotmail.com>
2017-09-05 21:28:17 +0200

    Database klasse aangevuld zodat database gevuld wordt met zelfde testdata als in Workshop1

--- src/main/java/workshop2/Database.java
+++ src/main/java/workshop2/Database.java
@@ -5,13 +5,21 @@
  */
 package workshop2;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import workshop2.domain.Account;
 import static workshop2.domain.AccountType.*;
+import workshop2.domain.Address;
+import static workshop2.domain.AddressType.*;
 import workshop2.domain.Customer;
+import workshop2.domain.Order;
+import workshop2.domain.OrderItem;
+import static workshop2.domain.OrderStatus.*;
+import workshop2.domain.Product;
 import workshop2.interfacelayer.DatabaseConnection;
 import workshop2.interfacelayer.controller.PasswordHash;
 
@@ -71,10 +79,95 @@ public class Database {
         em.persist(customer3);
         em.persist(customer4);
         em.persist(customer5);
-        et.commit();
+        
+        // Address
+        Address address1 = new Address("Postweg", 201, "h", "3781JK", "Aalst", customer1, POSTADRES);
+        Address address2 = new Address("Snelweg", 56, null, "3922JL", "Ee", customer2, POSTADRES);
+        Address address3 = new Address("Torenstraat", 82, null, "7620CX", "Best", customer2, FACTUURADRES);
+        Address address4 = new Address("Valkstraat", 9, "e", "2424DF", "Goorle", customer2, BEZORGADRES);
+        Address address5 = new Address("Dorpsstraat", 5, null, "9090NM", "Best", customer3, POSTADRES);
+        Address address6 = new Address("Plein", 45, null, "2522BH", "Oss", customer4, POSTADRES);
+        Address address7 = new Address("Maduralaan", 23, null, "8967HJ", "Apeldoorn", customer5, POSTADRES);
+        em.persist(address1);
+        em.persist(address2);
+        em.persist(address3);
+        em.persist(address4);
+        em.persist(address5);
+        em.persist(address6);
+        em.persist(address7);
+        
+        // Order
+        Order order1 = new Order(new BigDecimal("230.78"), customer1, LocalDateTime.now(), AFGEHANDELD);
+        Order order2 = new Order(new BigDecimal("62.97"), customer1, LocalDateTime.now(), AFGEHANDELD);
+        Order order3 = new Order(new BigDecimal("144.12"), customer1, LocalDateTime.now(), IN_BEHANDELING);
+        Order order4 = new Order(new BigDecimal("78.23"), customer2, LocalDateTime.now(), AFGEHANDELD);
+        Order order5 = new Order(new BigDecimal("6.45"), customer3, LocalDateTime.now(), NIEUW);
+        Order order6 = new Order(new BigDecimal("324.65"), customer3, LocalDateTime.now(), AFGEHANDELD);
+        Order order7 = new Order(new BigDecimal("46.08"), customer3, LocalDateTime.now(), IN_BEHANDELING);
+        Order order8 = new Order(new BigDecimal("99.56"), customer4, LocalDateTime.now(), NIEUW);
+        Order order9 = new Order(new BigDecimal("23.23"), customer5, LocalDateTime.now(), AFGEHANDELD);
+        em.persist(order1);
+        em.persist(order2);
+        em.persist(order3);
+        em.persist(order4);
+        em.persist(order5);
+        em.persist(order6);
+        em.persist(order7);
+        em.persist(order8);
+        em.persist(order9);
 
-        // Clear the persistency context
-        em.clear();
+        // Product
+        Product product1 = new Product("Goudse belegen kaas", new BigDecimal("12.99"), 134);
+        Product product2 = new Product("Goudse extra belegen kaas", new BigDecimal("14.70"), 239);
+        Product product3 = new Product("Leidse oude kaas", new BigDecimal("14.65"), 89);
+        Product product4 = new Product("Schimmelkaas", new BigDecimal("11.74"), 256);
+        Product product5 = new Product("Leidse jonge kaas", new BigDecimal("11.24"), 122);
+        Product product6 = new Product("Boeren jonge kaas", new BigDecimal("12.57"), 85);
+        em.persist(product1);
+        em.persist(product2);
+        em.persist(product3);
+        em.persist(product4);
+        em.persist(product5);
+        em.persist(product6);
+        
+        
+        // OrderItem
+        OrderItem orderItem1 = new OrderItem(order1, product6, 23, new BigDecimal("254.12"));
+        OrderItem orderItem2 = new OrderItem(order1, product1, 26, new BigDecimal("345.20"));
+        OrderItem orderItem3 = new OrderItem(order1, product2, 2, new BigDecimal("24.14"));
+        OrderItem orderItem4 = new OrderItem(order2, product1, 25, new BigDecimal("289.89"));
+        OrderItem orderItem5 = new OrderItem(order3, product4, 2, new BigDecimal("34.89"));
+        OrderItem orderItem6 = new OrderItem(order4, product2, 13, new BigDecimal("156.76"));
+        OrderItem orderItem7 = new OrderItem(order4, product5, 2, new BigDecimal("23.78"));
+        OrderItem orderItem8 = new OrderItem(order5, product2, 2, new BigDecimal("21.34"));
+        OrderItem orderItem9 = new OrderItem(order6, product1, 3, new BigDecimal("35.31"));
+        OrderItem orderItem10 = new OrderItem(order6, product3, 1, new BigDecimal("11.23"));
+        OrderItem orderItem11 = new OrderItem(order7, product6, 1, new BigDecimal("14.23"));
+        OrderItem orderItem12 = new OrderItem(order7, product2, 3, new BigDecimal("31.87"));
+        OrderItem orderItem13 = new OrderItem(order8, product4, 23, new BigDecimal("167.32"));
+        OrderItem orderItem14 = new OrderItem(order9, product1, 1, new BigDecimal("11.34"));
+        OrderItem orderItem15 = new OrderItem(order9, product2, 2, new BigDecimal("22.41"));
+        em.persist(orderItem1);
+        em.persist(orderItem2);
+        em.persist(orderItem3);
+        em.persist(orderItem4);
+        em.persist(orderItem5);
+        em.persist(orderItem6);
+        em.persist(orderItem7);
+        em.persist(orderItem8);
+        em.persist(orderItem9);
+        em.persist(orderItem10);
+        em.persist(orderItem11);
+        em.persist(orderItem12);
+        em.persist(orderItem13);
+        em.persist(orderItem14);
+        em.persist(orderItem15);
+        
+        
+        et.commit();
+        
+        // Close the persistency context
+        em.close();
     }
 //        // Prepare the SQL statements to create the DATABASE and recreate it
 //        String createDatabase = "CREATE DATABASE IF NOT EXISTS " + databaseName;
