20cb7cd6c5d624e29e2ce8db19fe865071d2c131
AhmedAlaaq <Ahmed_t9802004@yahoo.com>
2017-09-06 12:11:17 +0200

    Add files via upload

---
+++ src/main/java/workshop2/persistencelayer/hibernate/AddressServiceHibernate.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop2.interfacelayer.persistencelayer.hibernate;
+
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import workshop2.domain.Address;
+import workshop2.interfacelayer.DatabaseConnection;
+import workshop2.interfacelayer.persistencelayer.AddressService;
+import workshop2.interfacelayer.persistencelayer.GenericDaoImpl;
+
+
+/**
+ *
+ * @author Al-Alaaq(Egelantier)
+ */
+public class AddressServiceHibernate extends GenericServiceHibernate implements AddressService {
+
+    private static final Logger log = LoggerFactory.getLogger(AddressServiceHibernate.class);
+
+    private EntityManager entityManager;
+
+    private GenericDaoImpl addressDao;
+
+    public AddressServiceHibernate() {
+
+        entityManager = DatabaseConnection.getInstance().getEntityManager();
+
+        addressDao = new GenericDaoImpl(Address.class, entityManager);
+
+    }
+
+    @Override
+    public void createAddress(Address adres) {
+
+        try {
+
+            entityManager.getTransaction().begin();
+
+            addressDao.persist(adres);
+
+            entityManager.getTransaction().commit();
+
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+
+    @Override
+    public void deleteAddress(Address adres) {
+        try {
+
+            entityManager.getTransaction().begin();
+
+            addressDao.delete(adres);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+
+    @Override
+    public void updateAddress(Address adres) {
+        try {
+
+            entityManager.getTransaction().begin();
+
+            addressDao.update(adres);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+
+    @Override
+    public Optional<Address> findAddressByCustomerId(int id) {
+
+        Address adres;
+        String sql = "select i from Address i  where Customer.id = :id";
+
+        try {
+            Query query = entityManager.createNamedQuery(sql);
+            query.setParameter("Customer.id", id);
+
+            adres = (Address) query.getSingleResult();
+
+        } catch (NoResultException ex) {
+
+            log.debug("The id for customer {} is not found in the database", id);
+
+            return Optional.empty();
+
+        } finally {
+
+            entityManager.close();
+
+        }
+
+        return Optional.ofNullable(adres);
+
+    }
+
+    @Override
+    public List<Address> findAllAddressByCustomerId(int id) {
+
+        List<Address> addresses = null;
+        String sql = "select i from Address i  where Customer.id = :id";
+
+        try {
+            Query query = entityManager.createNamedQuery(sql);
+            query.setParameter("Customer.id", id);
+
+            return addresses = (List<Address>) query.getResultList();
+
+        } catch (NoResultException ex) {
+            log.debug("The id for customer {} is not found in the database", id);
+            return null;
+        }
+
+    }
+
+}
---
+++ src/main/java/workshop2/persistencelayer/hibernate/ProductServiceHibernate.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop2.interfacelayer.persistencelayer.hibernate;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import workshop2.domain.Product;
+import workshop2.interfacelayer.DatabaseConnection;
+import workshop2.interfacelayer.persistencelayer.GenericDaoImpl;
+import workshop2.interfacelayer.persistencelayer.ProductService;
+
+/**
+ *
+ * @author Al-Alaaq(Egelantier)
+ */
+public class ProductServiceHibernate extends GenericServiceHibernate implements ProductService {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceHibernate.class);
+
+    private EntityManager entityManager;
+
+    private GenericDaoImpl productDao;
+
+    public ProductServiceHibernate() {
+
+        entityManager = DatabaseConnection.getInstance().getEntityManager();
+
+        productDao = new GenericDaoImpl(Product.class, entityManager);
+
+    }
+
+    @Override
+    public void createProduct(Product product) {
+
+        try {
+
+            entityManager.getTransaction().begin();
+
+            productDao.persist(product);
+
+            entityManager.getTransaction().commit();
+
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+
+    @Override
+    public void deleteProduct(Product product) {
+        try {
+
+            entityManager.getTransaction().begin();
+
+            productDao.delete(product);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        try {
+
+            entityManager.getTransaction().begin();
+
+            productDao.update(product);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+
+            entityManager.getTransaction().rollback();
+
+            System.out.println("Transactie is niet uitgevoerd!");
+
+            // Exception doorgooien of FailedTransaction oid opgooien?
+        } finally {
+
+            // Always clear the persistence context to prevent increasing memory ????
+            entityManager.close();
+
+        }
+
+    }
+@Override
+    public List<Product> getAllProductsAsList() {
+        List<Product> products = null;
+        String sql = "Select * FROM Product ORDER BY name asc";
+        Query query = entityManager.createNamedQuery(sql);
+        return products = (List<Product>) query.getResultList();
+
+    }
+
+}
