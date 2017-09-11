989c16a12c1e991f8ff66140f17f4bda981bfeb9
AhmedAlaaq <Ahmed_t9802004@yahoo.com>
2017-09-11 17:48:31 +0200

    Update AddressController.java

--- src/main/java/workshop2/interfacelayer/controller/AddressController.java
+++ src/main/java/workshop2/interfacelayer/controller/AddressController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import workshop2.domain.Address;
+import workshop2.domain.Customer;
 import workshop2.persistencelayer.AddressService;
 import workshop2.persistencelayer.AddressServiceFactory;
 import workshop2.interfacelayer.view.AddressView;
@@ -20,21 +21,20 @@ import workshop2.interfacelayer.view.AddressView;
  * @author Ahmed Al-alaaq(Egelantier)
  */
 public class AddressController {
+
     private static final Logger log = LoggerFactory.getLogger(AddressController.class);
     private final AddressView addressView;
     private Address address;
-    private final  AddressService addressService = AddressServiceFactory.getAddressService();
-  
-    
-    public AddressController(AddressView addressView) {       
-        
-          this.addressView = addressView;
+    private final AddressService addressService = AddressServiceFactory.getAddressService();
 
-       
+    //private final CustomerService customerService = CustomerServiceFactory.getCustomerService();
+    public AddressController(AddressView addressView) {
+
+        this.addressView = addressView;
+
     }
-    
-    
-    public void createAddress(CustomerController customerController) {        
+
+    public void createAddress(CustomerController customerController) {
         // We first need a valid customer to link to the new address
         addressView.showConstructAddressStartScreen();
         Long customerId = customerController.selectCustomerIdByUser();
@@ -47,8 +47,10 @@ public class AddressController {
         if (listAddresses.size() == 3) {
             addressView.showNoAddressCanBeAdded();
             return;
-        }        
-        Optional<Address> optionalAddress = addressView.constructAddress(customerId, getAvailableAddressTypes());   
+        }
+        Optional<Customer> optionalCustomer = customerController.searchCustomerById(customerId);
+        Customer customer = optionalCustomer.get();
+        Optional<Address> optionalAddress = addressView.constructAddress(customer, getAvailableAddressTypes());
         if (optionalAddress.isPresent()) {
             // If the address type already exists we have to abort
             for (Address address : listAddresses) {
@@ -57,10 +59,11 @@ public class AddressController {
                     return;
                 }
             }
-            addressService.createAddress(optionalAddress.get());
-        }       
+            Address address = (Address)optionalAddress.get();
+            addressService.createAddress(address);
+        }
     }
-    
+
     public void deleteAddress(CustomerController customerController) {
         // We first need a valid customer to find the address to be deleted
         addressView.showDeleteAddressStartScreen();
@@ -73,9 +76,9 @@ public class AddressController {
         Optional<Address> optionalAddress = addressView.selectAddressToDelete(listAddresses);
         if (optionalAddress.isPresent()) {
             addressService.deleteAddress(optionalAddress.get());
-        }            
+        }
     }
-    
+
     public void updateAddress(CustomerController customerController) {
         // We first need a valid customer to find the address to be deleted
         addressView.showUpdateAddressStartScreen();
@@ -88,26 +91,23 @@ public class AddressController {
         Optional<Address> optionalAddress = addressView.selectAddressToUpdate(listAddresses);
         if (optionalAddress.isPresent()) {
             addressService.updateAddress(optionalAddress.get());
-        }            
-        
+        }
+
     }
-    
-   
-    
+
     List<Address> listAllAddressesFromCustomer(Long customerId) {
 
         return addressService.findAllAddressesByCustomerId(customerId);
-        
-               
+
     }
-    
+
     List<String> getAvailableAddressTypes() {
         List<String> addressType = new ArrayList<>();
         addressType.add("POSTADRES");
         addressType.add("FACTUURADRES");
         addressType.add("BEZORGADRES");
-        
+
         return addressType;
     }
-    
+
 }
